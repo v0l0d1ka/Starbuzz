099e90ccd4e6646af4d281f3ff6a0a1869f46679
v0l0d1ka <vovag777@mail.ru>
2019-05-18 06:37:16 +0300

    second commit - Drink Activity

--- app/src/main/java/com/best4u/vladimir/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/best4u/vladimir/starbuzz/DrinkActivity.java
@@ -2,15 +2,33 @@ package com.best4u.vladimir.starbuzz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DrinkActivity extends Activity {
 
-    public static final String EXTRA_DRINKID = "drinkId";
+    public static final String EXTRA_DRINK_ID = "drinkId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+        //for first commit just added
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINK_ID);
+        Drink drink = Drink.drinks[drinkId];
+
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+        ImageView photo = (ImageView) findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+
     }
-    //for first commit just added
+
 }
